bb7c29a3f4501f7a89ba650e75839d5a32b304f2
Farizkey16 <farizky.priambudi@gmail.com>
2025-06-20 10:51:52 +0700

    layouting fix

--- src/app/teams/page.tsx
+++ src/app/teams/page.tsx
@@ -61,9 +61,8 @@ const teamMembersR2: any[] = [
 export default function TeamsPage() {
 
     return (
-        <div>
+        <>
         <Navbar/>
-
         <section className="relative w-full h-[60vh] md:h-[80vh] pt-20">
 
 
@@ -140,13 +139,7 @@ export default function TeamsPage() {
 
 
         </section>
-
-
-
-
-
-
-        </div>
+        </>
 
 
 
